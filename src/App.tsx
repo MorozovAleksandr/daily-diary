bd120577e9081d24a19d472fb6e19062c62388e1
MorozovAleksandr <iamaleks.morozov@gmail.com>
2023-03-05 05:47:27 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from './components/primitives/Button';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <Button text={'test01'} />
+    </div>
+  );
+};
+
+export default App;
